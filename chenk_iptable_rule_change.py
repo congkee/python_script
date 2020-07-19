a33a3e960a8686f1254977d16afafe3d76b79ad6
mumengyun <749287428@qq.com>
2020-07-19 16:19:52 +0800

    兼容python2

--- chenk_iptable_rule_change.py
+++ chenk_iptable_rule_change.py
@@ -1,14 +1,17 @@
+#!/usr/bin/python2.7
 # coding: utf-8
 """
-用于监测iptables INPUT规则变动,并指出具体的变动规则
+用于监测iptables INPUT规则变动,并记录具体的变动规则
 """
+import json
+import logging
 import os
 import subprocess
 import time
 import urllib2
 
-
-# import requests --python3+
+LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+logging.basicConfig(filename='/tmp/iptables_rule_change.log', level=logging.INFO, format=LOG_FORMAT)
 
 
 def old_rules():
@@ -39,25 +42,19 @@ def new_rules():
 
 
 def diff_rules():
-    diff_add = list(set(new_rules()).difference(set(old_rules())))
-    diff_del = list(set(old_rules()).difference(set(new_rules())))
+    new_list = new_rules()
+    old_list = old_rules()
+    diff_add = list(set(new_list).difference(set(old_list)))
+    diff_del = list(set(old_list).difference(set(new_list)))
 
-    if len(diff_add) != 0:
-        status = "新增规则"
-        value = 0
-        change_rules = diff_add
-    elif len(diff_del) != 0:
-        status = "删除规则"
-        value = 0
-        change_rules = diff_del
-    else:
-        status = ""
+    if len(diff_add) == len(diff_del) == 0:
         value = 1
-        change_rules = []
+    else:
+        value = 0
+        logging.info("此次防火墙规则变动,删除了规则{diff_del},新增了规则{diff_add},请评估！！！".format(diff_del=diff_del, diff_add=diff_add))
+
     data = {
-        "status": status,
-        "value": value,
-        "change_rules": change_rules
+        "value": value
     }
 
     return data
@@ -65,24 +62,22 @@ def diff_rules():
 
 def push_falcon():
     ts = int(time.time())
-    data = diff_rules()
-    hostname = subprocess.Popen(
-        'cat /usr/local/falcon-agent/config/cfg.json | grep hostname | awk -F "\"" "{print $4}"', shell=True,
-        stdout=subprocess.PIPE).communicate()
-    print(hostname)
-    # hostname = "test"
-    push_data = {
+    diff_data = diff_rules()
+    hostname = json.load(open('/usr/local/falcon-agent/config/cfg.json', 'r'))['hostname'].encode('utf-8')
+    push_data = [{
         "endpoint": hostname,
-        "metric": "iptables_rule",
+        "metric": "iptables_rule_change",
         "timestamp": ts,
         "step": 60,
-        "value": data['value'],
+        "value": diff_data['value'],
         "counterType": "GAUGE",
-        "tags": 'change_rules="{status}{change_rules}'.format(change_rules=data['change_rules'], status=data['status']),
-    }
-    print(push_data)
+        "tags": "role=iptables_rule_check"
+    }]
     # requests.post("http://127.0.0.1:1988/v1/push", data=push_data) --python3+
-    urllib2.Request("http://127.0.0.1:1988/v1/push", data=push_data)
+    url = "http://127.0.0.1:1988/v1/push"
+    request = urllib2.Request(url, json.dumps(push_data))
+    urllib2.urlopen(request)
+
     subprocess.Popen('iptables -nL INPUT > /tmp/iptables_rules_txt', shell=True).wait()  # 将现有新规则写入到文件,用作下次对比的旧规则
 
 
