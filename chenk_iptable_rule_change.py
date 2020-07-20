c9a993c46c61592414e465c38982404e4edb3955
mumengyun <749287428@qq.com>
2020-07-20 18:30:19 +0800

    bug修复

--- chenk_iptable_rule_change.py
+++ chenk_iptable_rule_change.py
@@ -29,6 +29,7 @@ def old_rules():
         with open(iptables_rules_txt, 'r') as f:
             for line in f:
                 old_rules_list.append(line.strip('\n'))
+    logging.debug(old_rules_list)
     return old_rules_list
 
 
@@ -38,21 +39,26 @@ def new_rules():
     """
     rules = subprocess.Popen('iptables -nL INPUT ', shell=True, stdout=subprocess.PIPE).communicate()
     new_rules_list = rules[0].split('\n')
+    logging.debug(new_rules_list)
     return new_rules_list
 
 
 def diff_rules():
-    new_list = list(filter(None, new_rules()))
+    new_list = list(filter(None, new_rules()))  # 排除列表中存在的空元素
     old_list = list(filter(None, old_rules()))
 
-    diff_add = list(set(new_list).difference(set(old_list)))
-    diff_del = list(set(old_list).difference(set(new_list)))
-
-    if len(diff_add) == len(diff_del) == 0:
+    if len(new_list) == 0 or len(old_list) == 0:
         value = 1
+        logging.info("此次防火墙规则变动,新的规则{new_list},旧的规则规则{old_list},请评估！".format(new_list=new_list, old_list=old_list))  # 这里的目的是为了防止获取不到防火墙规则,导致误报
     else:
-        value = 0
-        logging.info("此次防火墙规则变动,删除了规则{diff_del},新增了规则{diff_add},请评估！".format(diff_del=diff_del, diff_add=diff_add))
+        diff_add = list(set(new_list).difference(set(old_list)))
+        diff_del = list(set(old_list).difference(set(new_list)))
+
+        if len(diff_add) == len(diff_del) == 0:
+            value = 1
+        else:
+            value = 0
+            logging.info("此次防火墙规则变动,删除了规则{diff_del},新增了规则{diff_add},请评估！".format(diff_del=diff_del, diff_add=diff_add))
 
     data = {
         "value": value
