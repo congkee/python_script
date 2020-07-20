704e8cc6b80f96325e4191d14d177fe219ff7478
mumengyun <749287428@qq.com>
2020-07-20 11:29:13 +0800

    解决列表空值问题

--- chenk_iptable_rule_change.py
+++ chenk_iptable_rule_change.py
@@ -37,13 +37,14 @@ def new_rules():
     执行iptables -nL INPUT 的结果
     """
     rules = subprocess.Popen('iptables -nL INPUT ', shell=True, stdout=subprocess.PIPE).communicate()
-    new_rules_list = (rules[0].strip().split('\n'))
+    new_rules_list = rules[0].split('\n')
     return new_rules_list
 
 
 def diff_rules():
-    new_list = new_rules()
-    old_list = old_rules()
+    new_list = list(filter(None, new_rules()))
+    old_list = list(filter(None, old_rules()))
+
     diff_add = list(set(new_list).difference(set(old_list)))
     diff_del = list(set(old_list).difference(set(new_list)))
 
@@ -51,7 +52,7 @@ def diff_rules():
         value = 1
     else:
         value = 0
-        logging.info("此次防火墙规则变动,删除了规则{diff_del},新增了规则{diff_add},请评估！！！".format(diff_del=diff_del, diff_add=diff_add))
+        logging.info("此次防火墙规则变动,删除了规则{diff_del},新增了规则{diff_add},请评估！".format(diff_del=diff_del, diff_add=diff_add))
 
     data = {
         "value": value
@@ -77,7 +78,6 @@ def push_falcon():
     url = "http://127.0.0.1:1988/v1/push"
     request = urllib2.Request(url, json.dumps(push_data))
     urllib2.urlopen(request)
-
     subprocess.Popen('iptables -nL INPUT > /tmp/iptables_rules_txt', shell=True).wait()  # 将现有新规则写入到文件,用作下次对比的旧规则
 
 
